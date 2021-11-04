f0954e7c2a69d0d5433ae79e91c11d21ce915f48
Renan Oliveira <renano@motorola.com>
2021-11-03 21:46:19 -0300

    PEP8 changes

--- tasks/test_four.py
+++ tasks/test_four.py
@@ -8,9 +8,9 @@ def test_asdict():
     t_task = Task('do something', 'okken', True, 21)
     t_dict = t_task._asdict()
     expected = {'summary': 'do something',
-                'owner': 'okken',
-                'done': True,
-                'id': 21}
+    'owner': 'okken',
+    'done': True,
+    'id': 21}
     assert t_dict == expected
 
 def test_replace():
